0de074ec3cbcc581b1cd830e32f7c16297f16e36
ttoru96 <ttoru96jk@gmail.com>
2021-05-02 15:19:20 +0900

    Submission template customized for google codejam

---
+++ 2021-women/irrefutable_outcome.py
@@ -0,0 +1,9 @@
+def solve(s):
+    return s
+
+
+if __name__ == "__main__":
+    T = int(input()) # the number of testcases
+    for i in range(T):
+        s = input()
+        print(f'Case #{T}: {solve(s)}')
